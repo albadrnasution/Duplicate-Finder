c988bd614c1f82435991722ba77c208c3da1630e
albadrnasution <m.ln.albadr@gmail.com>
2019-08-12 12:29:12 +0900

    lets try to our recovery photos

--- dufinder/dupfinder.go
+++ dufinder/dupfinder.go
@@ -10,25 +10,42 @@ import (
 )
 
 func main() {
-	target := "D:/Photos"
+	base := "D:/Photos2"
+	target := "L:/F-recovery"
 	start := time.Now()
 	startFormatted := start.Format("2006-01-02 15.04.05")
-	resultPath := "Result" + startFormatted + ".dat"
 
-	fmt.Printf("Start hashing %s outputting to %s\n", target, resultPath)
-	outFile, err := os.Create(resultPath)
+	/*
+		resultPath := "Result" + startFormatted + ".dat"
+		fmt.Printf("Start hashing %s outputting to %s\n", target, resultPath)
+		outFile, err := os.Create(resultPath)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer outFile.Close()
+
+		allmap := dir.CollectBySingleChannel(target)
+		w := bufio.NewWriter(outFile)
+		for kHash, vPath := range allmap {
+			line := kHash + ", " + vPath
+			fmt.Fprintln(w, line)
+		}
+		w.Flush()
+	*/
+
+	dupPath := "Dup Result" + startFormatted + ".dat"
+	dupResult := dir.FindDuplicate(base, target)
+
+	drOutFile, err := os.Create(dupPath)
 	if err != nil {
 		fmt.Println(err)
 	}
-	defer outFile.Close()
+	defer drOutFile.Close()
+	drWriter := bufio.NewWriter(drOutFile)
+	dir.WriteDuplicateResult(dupResult, drWriter)
+	drWriter.Flush()
 
-	allmap := dir.CollectBySingleChannel(target)
-	w := bufio.NewWriter(outFile)
-	for kHash, vPath := range allmap {
-		line := kHash + ", " + vPath
-		fmt.Fprintln(w, line)
-	}
-	w.Flush()
 	elapsed := time.Since(start)
-	log.Printf("Hashing %s took %s\n", target, elapsed)
+	//log.Printf("Hashing %s took %s\n", target, elapsed)
+	log.Printf("Hashing %s and %s took %s\n", base, target, elapsed)
 }
